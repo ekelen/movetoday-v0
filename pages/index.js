778f4367498fe7b7690b8b09405ba245dd740dc7
Elizabeth Kelen <erskelen@gmail.com>
2021-02-03 12:39:37 -0500

    lay out all list horizontally

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { xorBy } from "lodash";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Move from "../src/components/move";
 import { foci } from "../src/data/constants";
 import moveList from "../src/data/moveList.json";
@@ -27,48 +27,33 @@ const Home = ({ content }) => {
   };
 
   return (
-    <div className="w-full flex p-5 items-start">
+    <div className="border-2 border-gray-600 h-screen bg-red-100 overflow-x-scroll">
       <button
         onClick={chooseRandom}
         className="bg-gray-800 text-white text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
       >
         move!
       </button>
-      {/* All */}
-      <div className="w-1/2">
-        <div className="w-full flex flex-wrap flex-grow flex-shrink-0 space-x-1 space-y-2">
-          <h3 className="font-display w-full">
-            All moves (n = {allMoves.length})
-          </h3>
-          {foci.map((focus, i) => {
-            return (
-              <div
-                key={`${focus}-${i}`}
-                className="flex flex-wrap space-x-2 space-y-2 items-start"
-              >
-                <h4 className="font-display bg-blue-600 bg-opacity-30 w-min">
-                  {focus}
-                </h4>
-                {allMoves
-                  .filter((m) => m.focus.includes(focus))
-                  .map((m, i) => {
-                    return (
-                      <div
-                        className="pt-2 p-1 bg-blue-200 bg-opacity-20 border-black border-b-2"
-                        onClick={() => toggleMove(m)}
-                        key={`${m.name}-${i}`}
-                      >
-                        <Move move={{ name: m.name }} />
-                      </div>
-                    );
-                  })}
+      <div className="overflow-x-scroll grid grid-rows-3 grid-flow-col auto-cols-max gap-2">
+        {/* All */}
+        {allMoves.map((m, i) => {
+          return (
+            <div
+              className="relative pt-2 p-1 bg-blue-200 border-black border-b-2"
+              onClick={() => toggleMove(m)}
+              key={`${m.name}-${i}`}
+            >
+              <Move move={{ name: m.name }} />
+              <div className="absolute bottom-0 right-0 text-xs bg-purple-600 text-white p-1">
+                {m.focus}
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
+
       {/* Selected */}
-      <div className="w-1/2 flex flex-wrap space-y-3 space-x-2 items-start flex-start content-start">
+      <div className="w-full h-4/6 flex flex-wrap space-y-3 space-x-2 items-start flex-start content-start">
         <h3 className="font-display w-full">
           Selected moves (n = {selectedMoves.length})
         </h3>
