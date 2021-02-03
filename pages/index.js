7289859f24b5b95cce828fc60a5a1214d9dc5f2d
Elizabeth Kelen <erskelen@gmail.com>
2021-02-03 13:22:03 -0500

    remove horizontal scroll on basic edit mode finish

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import { xorBy } from "lodash";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Move from "../src/components/move";
 import { foci } from "../src/data/constants";
 import moveList from "../src/data/moveList.json";
 import { bloatDataInMemory } from "../src/util/test.js";
 import { randInt } from "../src/util/util";
 
+import { useRouter } from "next/router";
+
 const Home = ({ content }) => {
+  // const router = useRouter();
   const allMoves =
     process.env.NEXT_PUBLIC_LIGHTHOUSE === "on"
       ? bloatDataInMemory(moveList)
       : moveList;
-  const [selectedMoves, setSelectedMoves] = useState([]);
+  const [selectedMoves, setSelectedMoves] = useState([allMoves[0]]);
+  const [editMode, setEditMode] = useState(true);
 
   const toggleMove = (move) =>
-    setSelectedMoves(xorBy(selectedMoves, [move], "id"));
+    editMode && setSelectedMoves(xorBy(selectedMoves, [move], "id"));
+
   const getMovesForFocus = (focus) =>
     allMoves.filter((m) => m.focus.includes(focus));
 
@@ -26,6 +31,16 @@ const Home = ({ content }) => {
     setSelectedMoves(randomMoves);
   };
 
+  const onFinalize = () => {
+    // router.push(`/?editmode=false`, undefined, { shallow: true });
+    setEditMode(false);
+  };
+
+  const onEdit = () => {
+    // router.push(`/?editmode=true`, undefined, { shallow: true });
+    setEditMode(true);
+  };
+
   return (
     <div className="h-screen overflow-hidden border-2 border-gray-600 bg-red-100 ">
       <button
@@ -34,7 +49,14 @@ const Home = ({ content }) => {
       >
         move!
       </button>
-      <div className="overflow-x-scroll scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300  grid grid-rows-3 grid-flow-col auto-cols-max gap-2">
+
+      <div
+        className={`${
+          editMode
+            ? "overflow-x-scroll scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300"
+            : "relative overflow-hidden"
+        }  grid grid-rows-3 grid-flow-col auto-cols-max gap-2`}
+      >
         {/* All */}
         {allMoves.map((m, i) => {
           return (
@@ -53,7 +75,13 @@ const Home = ({ content }) => {
       </div>
 
       {/* Selected */}
-      <div className="w-full h-4/6 flex flex-wrap space-y-3 space-x-2 items-start flex-start content-start">
+      <div
+        className={`${
+          editMode
+            ? "w-full h-4/6 flex flex-wrap space-y-3 space-x-2 items-start flex-start content-start"
+            : "fixed top-0 left-0 right-0 bottom-0 overflow-y-scroll bg-blue-700"
+        }`}
+      >
         <h3 className="font-display w-full">
           Selected moves (n = {selectedMoves.length})
         </h3>
@@ -97,6 +125,15 @@ const Home = ({ content }) => {
             );
           })}
       </div>
+      {true && (
+        <button
+          onClick={editMode ? onFinalize : onEdit}
+          //
+          className="sticky bottom-0 right-0 bg-gray-800 text-white text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+        >
+          {editMode ? "done!" : "x"}
+        </button>
+      )}
     </div>
   );
 };
