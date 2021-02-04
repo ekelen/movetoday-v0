7a996b09e7ac532ddfb06b5c1a9f889eec6b3e1b
Elizabeth Kelen <erskelen@gmail.com>
2021-02-04 13:48:15 -0500

    remove dark text and blue debug bg

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,34 @@
-import { xorBy } from "lodash";
-import { Fragment, useEffect, useState } from "react";
+import { sampleSize, xorBy } from "lodash";
+import { useEffect, useState } from "react";
 import Move from "../src/components/move";
-import { foci } from "../src/data/constants";
+import meta from "../src/data/meta.json";
 import moveList from "../src/data/moveList.json";
 import { bloatDataInMemory } from "../src/util/test.js";
-import { randInt } from "../src/util/util";
-
-import { useRouter } from "next/router";
 
 const Home = ({ content }) => {
-  // const router = useRouter();
   const allMoves =
     process.env.NEXT_PUBLIC_LIGHTHOUSE === "on"
       ? bloatDataInMemory(moveList)
       : moveList;
-  const [selectedMoves, setSelectedMoves] = useState([allMoves[0]]);
+
+  const [selectedMoves, setSelectedMoves] = useState(
+    meta.defaults.map((slug) => allMoves.find((mv) => mv.slug === slug))
+  );
+
   const [editMode, setEditMode] = useState(true);
 
   const toggleMove = (move) =>
     editMode && setSelectedMoves(xorBy(selectedMoves, [move], "id"));
 
-  const getMovesForFocus = (focus) =>
-    allMoves.filter((m) => m.focus.includes(focus));
-
   const chooseRandom = () => {
-    const randomMoves = foci
-      .map((focus) => getMovesForFocus(focus))
-      .filter((movesByFocus) => movesByFocus.length)
-      .map((movesByFocus) => movesByFocus[randInt(movesByFocus.length)]);
+    const randomMoves = sampleSize(moveList, 10);
     setSelectedMoves(randomMoves);
+    onFinalize();
   };
 
+  useEffect(() => {
+    console.log(`selectedMoves:`, selectedMoves);
+  }, [selectedMoves]);
   const onFinalize = () => {
     // router.push(`/?editmode=false`, undefined, { shallow: true });
     setEditMode(false);
@@ -42,18 +40,17 @@ const Home = ({ content }) => {
   };
 
   return (
-    <div className="h-screen overflow-hidden border-2 border-gray-600">
+    <div className="h-screen relative overflow-hidden border-2 border-gray-600">
       <button
         onClick={chooseRandom}
-        className="bg-gray-800 text-white text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+        className="w-min bg-yellow-700 text-yellow-100 text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
       >
-        move!
+        random!
       </button>
-
       <div
         className={`${
           editMode
-            ? "overflow-x-scroll scrollbar scrollbar-thumb-yellow-900 scrollbar-track-yellow-600"
+            ? "relative overflow-x-scroll scrollbar scrollbar-thumb-yellow-900 scrollbar-track-yellow-600"
             : "relative overflow-hidden"
         }  p-5 grid grid-rows-3 grid-flow-col auto-cols-max gap-2`}
       >
@@ -61,6 +58,7 @@ const Home = ({ content }) => {
         {allMoves.map((m, i) => {
           return (
             <Move
+              key={`${m.name}-${i}`}
               onClick={() => toggleMove(m)}
               move={{ name: m.name }}
               tags={[m.focus]}
@@ -74,53 +72,32 @@ const Home = ({ content }) => {
         className={`${
           editMode
             ? "w-full h-4/6 flex flex-wrap space-y-3 space-x-2 items-start flex-start content-start"
-            : "fixed top-0 left-0 right-0 bottom-0 overflow-y-scroll bg-blue-700"
+            : "fixed top-0 left-0 right-0 bottom-0 overflow-y-scroll bg-gray-900"
         }`}
       >
-        <h3 className="font-display w-full">
+        <h3 className="text-yellow-100 pt-7 p-3 font-display w-full">
           Selected moves (n = {selectedMoves.length})
         </h3>
-        {foci
-          .filter((focus) => selectedMoves.some((m) => m.focus.includes(focus)))
-          .map((focus, i) => {
-            return (
-              <div
-                key={`${focus}-${i}`}
-                className="flex flex-wrap space-x-2 space-y-2 items-start"
-              >
-                <h4 className="font-display bg-blue-600 bg-opacity-60 w-min">
-                  {focus}
-                </h4>
-                {selectedMoves
-                  .filter((m) => m.focus.includes(focus))
-                  .map((m, i) => {
-                    const {
-                      name,
-                      repsMin,
-                      repsMax,
-                      durationMin,
-                      durationMax,
-                      sets,
-                    } = m;
-                    return (
-                      <Move
-                        key={`${m.name}-${i}`}
-                        onClick={() => toggleMove(m)}
-                        move={{
-                          name,
-                        }}
-                      />
-                    );
-                  })}
-              </div>
-            );
-          })}
+
+        {selectedMoves.map((m, i) => {
+          const { name, repsMin, repsMax, durationMin, durationMax, sets } = m;
+
+          return (
+            <Move
+              key={`${m.name}-${i}`}
+              onClick={() => toggleMove(m)}
+              move={{
+                name,
+              }}
+              tags={m.focus.split(",")}
+            />
+          );
+        })}
       </div>
       {true && (
         <button
           onClick={editMode ? onFinalize : onEdit}
-          //
-          className="sticky bottom-0 right-0 bg-gray-800 text-white text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+          className="absolute bottom-0 right-0 bg-yellow-800 text-yellow-100 text-sm py-3 px-4 rounded font-bold flex items-center mr-4 hover:bg-yellow-500 focus:outline-none focus:bg-yellow-400"
         >
           {editMode ? "done!" : "x"}
         </button>
