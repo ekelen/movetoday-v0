46f8fcfa52915613db756f4d8c4f7b7b396bbfb6
Elizabeth Kelen <erskelen@gmail.com>
2021-02-05 00:09:52 -0500

    new style for moves in final mode

--- src/components/move.js
+++ src/components/move.js
@@ -1,37 +1,39 @@
 import { Fragment } from "react";
 import styles from "./move.module.css";
 
-const Move = ({ move, onClick, tags }) => {
-  // arbitrary for testing
-  const bgStyle =
-    move.name && move.name.charAt(0) < "m" ? styles.bg0 : styles.bg1;
-
+const Move = ({ move, onClick, tags, editMode = true, done = null }) => {
   return (
-    <Fragment>
-      <ul
-        className={`p-3 relative ${styles.test} ${bgStyle}`}
+    <div className="relative">
+      <button
+        className={`m-1 p-3 relative text-yellow-100 font-bold font-mono ${
+          editMode
+            ? styles.bg1
+            : done
+            ? "bg-green-900 bg-opacity-50"
+            : "bg-blue-900"
+        }`}
         onClick={onClick}
       >
         {Object.entries(move)
           .filter(([_, v]) => !!v)
           .map(([k, v], i) => (
-            <li key={`${k}-${i}`} className="text-xs whitespace-nowrap">
+            <span key={`${k}-${i}`} className="text-xs whitespace-nowrap">
               <span>{k}: </span>
               <span>{v}</span>
-            </li>
+            </span>
           ))}
-      </ul>
-      {tags &&
-        tags.length > 0 &&
-        tags.map((t, i) => (
-          <div
-            key={`${t}-${i}`}
-            className="absolute -bottom-1.5 -right-0.5 text-xs bg-purple-900 text-white p-0 font-light font-sans"
-          >
-            {t}
-          </div>
-        ))}
-    </Fragment>
+        {tags &&
+          tags.length > 0 &&
+          tags.map((t, i) => (
+            <div
+              key={`${t}-${i}`}
+              className="absolute -bottom-1.5 -right-0.5 text-xs bg-purple-900 text-white p-0 font-light font-sans"
+            >
+              {t}
+            </div>
+          ))}
+      </button>
+    </div>
   );
 };
 
