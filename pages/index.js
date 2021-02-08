95a815efc8260d2bf93429eb4290dc3680518c09
Elizabeth Kelen <erskelen@gmail.com>
2021-02-08 16:28:11 -0500

    add local allmoves

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,56 @@ import meta from "../src/data/meta.json";
 import moveList from "../src/data/moveList.json";
 import { bloatDataInMemory } from "../src/util/test.js";
 
+const AllMoves = ({
+  availableMoves,
+  setFocusFilter,
+  focusFilter,
+  setSearchFilter,
+  searchFilter,
+  toggleMove,
+  selectedMoves,
+  onChange,
+  onSearch,
+}) => {
+  const containerGrid =
+    "grid grid-rows-moveHeight grid-flow-col auto-cols-max gap-2";
+
+  const presetText =
+    "text-primary-100 bg-primary-800 text-xs px-2 py-1 rounded";
+  return (
+    <div className="relative overflow-hidden mx-5 my-2 p-3 rounded border-primary-400 border-2 ">
+      <div className="text-primary-400 text-xs uppercase">
+        1. Choose moves or use <span className={presetText}>default</span> or{" "}
+        <span className={presetText}>random</span>
+      </div>
+      {/* <AllMovesHeader
+          {...{
+            setFocusFilter,
+            setSearchFilter,
+            onChange,
+            onSearch,
+            searchFilter,
+            focusFilter,
+          }}
+        /> */}
+      <div
+        className={`${containerGrid} w-screen relative overflow-x-scroll scrollbar scrollbar-thumb-primary-800 scrollbar-track-primary-900 p-5`}
+      >
+        {availableMoves.map((m, i) => {
+          return (
+            <Move
+              key={`${m.name}-${i}`}
+              onClick={() => toggleMove(m)}
+              move={m}
+              selected={selectedMoves.map((m) => m.id).includes(m.id)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 const Home = ({ content }) => {
   const [initialMoveList] = useState(
     process.env.NEXT_PUBLIC_LIGHTHOUSE === "on"
@@ -98,87 +148,25 @@ const Home = ({ content }) => {
         <Fragment>
           <Header></Header>
           <div className="bg-primary-900 h-screen relative overflow-hidden border-2 border-gray-600 flex flex-wrap items-center">
-            <div className="flex items-center w-full space-x-3 px-3">
-              <input
-                type="text"
-                aria-label="search"
-                placeholder="🔎 search!"
-                onChange={onSearch}
-                value={searchFilter}
-                className={`w-min text-primary-100 bg-primary-700 text-sm py-2 px-3 rounded font-bold flex items-center self-bottom mr-4 hover:bg-primary-100`}
-              ></input>
-              {/* Dropdown */}
-              <select
-                className="px-2 py-1 rounded-full flex lg:hidden"
-                value={focusFilter}
-                onChange={onChange}
-                autoComplete="on"
-              >
-                {[...foci, "any"].map((focus, i) => {
-                  return (
-                    <option
-                      key={focus + `${i}`}
-                      disabled={focusFilter === focus}
-                      value={focus}
-                    >
-                      {focus}
-                    </option>
-                  );
-                })}
-              </select>
-
-              {/* Tags */}
-              <div className="hidden lg:flex space-x-2 items-center">
-                {[...foci, "any"].map((focus, i) => (
-                  <button
-                    key={`${focus}-${i}`}
-                    aria-label={`moves with ${focus} focus`}
-                    className={`${
-                      focus === focusFilter || (!focusFilter && focus === "any")
-                        ? "bg-primaryAction-500"
-                        : "bg-primary-400 opacity-60"
-                    }  whitespace-nowrap uppercase text-xs w-min py-1 px-2 rounded-full`}
-                    onClick={() => setFocusFilter(focus === "any" ? "" : focus)}
-                  >
-                    {focus}
-                  </button>
-                ))}
-              </div>
-
-              {/* Clear filters */}
-              <button
-                aria-label="clear filters"
-                disabled={
-                  !searchFilter && (!focusFilter || focusFilter === "any")
-                }
-                onClick={() => {
-                  setSearchFilter("");
-                  setFocusFilter("");
-                }}
-                className="w-min bg-primary-200 text-primary-700 text-sm py-2 px-3 rounded font-bold flex items-center mr-4 hover:bg-primary-500 disabled:opacity-30 disabled:hover:cursor-not-allowed"
-              >
-                reset
-              </button>
-            </div>
-            <div
-              className={`${
-                editMode
-                  ? "relative overflow-x-scroll scrollbar scrollbar-thumb-primary-800 scrollbar-track-primary-900"
-                  : "relative hidden"
-              }  p-5 grid grid-rows-3 grid-flow-col auto-cols-max gap-2`}
-            >
+            <div className="contents">
               {/* All */}
 
-              {availableMoves.map((m, i) => {
-                return (
-                  <Move
-                    key={`${m.name}-${i}`}
-                    onClick={() => toggleMove(m)}
-                    move={m}
-                    selected={selectedMoves.map((m) => m.id).includes(m.id)}
-                  />
-                );
-              })}
+              <AllMoves
+                {...{
+                  searchFilter,
+                  setSearchFilter,
+                  selectedMoves,
+                  setSelectedMoves,
+                  onChange,
+                  availableMoves,
+                  focusFilter,
+                  setFocusFilter,
+                  onSearch,
+                  editMode,
+                  setEditMode,
+                  toggleMove,
+                }}
+              />
             </div>
 
             {/* Selected */}
