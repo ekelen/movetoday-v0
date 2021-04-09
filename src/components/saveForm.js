d85050babeee0ab0b8d1724e2d68469e9e4acf09
Elizabeth Kelen <erskelen@gmail.com>
2021-04-09 12:23:23 -0400

    split out save form component

---
+++ src/components/saveForm.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Check, X } from "react-feather";
+import { useFetchForHistory } from "../../state/history";
+
+const SaveForm = ({ children, ...props }) => {
+  const [apiKey, setApiKey] = useState("");
+
+  const [success, loading, updateHistory] = useFetchForHistory(apiKey, [
+    ...Object.keys(props.movesProgress),
+  ]);
+  const onEnterKey = (e) => {
+    setApiKey(e.target.value);
+  };
+
+  const onMakeRequest = () => {
+    updateHistory();
+  };
+
+  const formCn = "flex items-center relative";
+  const formCnDisabled =
+    "flex items-center relative opacity-30 cursor-default pointer-events-none";
+
+  return (
+    <div className={loading ? formCnDisabled : formCn}>
+      <input
+        id="api-key"
+        type="text"
+        aria-label="API Key"
+        placeholder={"Enter API key"}
+        onChange={onEnterKey}
+        value={apiKey}
+        disabled={loading}
+        className={`w-min mr-2 text-xs py-1 px-1 rounded-sm flex items-center self-bottom focus:outline-none focus:ring-2 focus:ring-yellow-300 text-primary-100 placeholder-gray-100 bg-primary-700 hover:bg-primary-600`}
+      ></input>
+      <p className="absolute text-xs -bottom-5">
+        {loading ? (
+          <span className="animate-pulse">Loading...</span>
+        ) : success === null ? (
+          ""
+        ) : success === false ? (
+          <span className="text-red-500">Error saving. Check API key.</span>
+        ) : (
+          <span>Success.</span>
+        )}
+      </p>
+      <button
+        onClick={onMakeRequest}
+        disabled={loading}
+        className="flex items-center bg-primary-800 rounded-lg py-1 px-2 text-xs mr-2 font-bold"
+      >
+        <Check size={18} /> <p className="ml-1">Submit</p>
+      </button>
+
+      <button
+        className="flex items-center bg-primary-800 rounded-lg py-1 px-2 text-xs mr-2 font-bold"
+        onClick={props.onCancel}
+        disabled={loading}
+      >
+        <X size={18} />
+        <p className="ml-1">Cancel</p>
+      </button>
+    </div>
+  );
+};
+export default SaveForm;
--- src/components/sequenceHeader.js
+++ src/components/sequenceHeader.js
@@ -1,9 +1,6 @@
 import { Fragment, useState } from "react";
-import { Check, Save, X } from "react-feather";
-
-import { useFetchForHistory } from "../../state/history";
-
-const LS_APIKEY = "LS_API_KEY";
+import { Save } from "react-feather";
+import SaveForm from "./saveForm";
 
 const Button = ({ onClick, disabled, children, ...props }) => {
   return (
@@ -25,21 +22,7 @@ const SequenceHeader = ({
   zeroAllProgress,
   ...props
 }) => {
-  const [apiKey, setApiKey] = useState("");
   const [showForm, setShowForm] = useState(false);
-  const [success, loading, updateHistory] = useFetchForHistory(apiKey, [
-    ...Object.keys(movesProgress),
-  ]);
-  const saveDisabled = process.env.NODE_ENV !== "development";
-
-  const onEnterKey = (e) => {
-    setApiKey(e.target.value);
-  };
-
-  const onMakeRequest = () => {
-    updateHistory();
-  };
-
   return (
     <Fragment>
       <div className="relative w-full p-2 flex items-center space-x-2">
@@ -50,17 +33,17 @@ const SequenceHeader = ({
           </p>
           clear all progress
         </Button>
-        {!showForm && (
-          <Button
-            onClick={() => setShowForm(true)}
-            // disabled={saveDisabled}
-          >
+
+        {showForm ? (
+          <SaveForm
+            onCancel={() => setShowForm(false)}
+            movesProgress={movesProgress}
+          />
+        ) : (
+          <Button onClick={() => setShowForm(true)}>
             <p
               className={
                 "relative font-mono mr-2 rounded-full bg-black text-primaryAction-600 h-6 w-6 px-1 py-1"
-                // process.env.NODE_ENV !== "development"
-                //   ? "relative font-mono mr-2 rounded-full bg-black text-primary-500 h-6 w-6 px-1 py-1"
-                //   : "relative font-mono mr-2 rounded-full bg-black text-primaryAction-600 h-6 w-6 px-1 py-1"
               }
             >
               <span className="h-full align-middle">
@@ -70,47 +53,6 @@ const SequenceHeader = ({
             {"save"}
           </Button>
         )}
-        {showForm && (
-          <div className="flex items-center relative">
-            <input
-              id="api-key"
-              type="text"
-              aria-label="API Key"
-              placeholder={"Enter API key"}
-              onChange={onEnterKey}
-              value={apiKey}
-              className={`w-min mr-2 text-xs py-1 px-1 rounded-sm flex items-center self-bottom focus:outline-none focus:ring-2 focus:ring-yellow-300 text-primary-100 placeholder-gray-100 bg-primary-700 hover:bg-primary-600`}
-            ></input>
-            <p className="absolute text-xs -bottom-5">
-              {success === null
-                ? "Enter API key"
-                : success === false
-                ? "Error saving. Check API key."
-                : "Success."}
-            </p>
-            <button
-              className="flex items-center bg-primary-800 rounded-lg py-1 px-2 text-xs mr-2 font-bold"
-              onClick={onMakeRequest}
-            >
-              <Check size={18} /> <p className="ml-1">Submit</p>
-            </button>
-
-            <button
-              className="flex items-center bg-primary-800 rounded-lg py-1 px-2 text-xs mr-2 font-bold"
-              onClick={() => setShowForm(false)}
-            >
-              <X size={18} />
-              <p className="ml-1">Cancel</p>
-            </button>
-          </div>
-        )}
-        <div className="flex-auto ml-auto mr-0 flex justify-end text-primary-300 border-2 hidden">
-          {/* <p>{error && `Error: ${error}`}</p>
-          <p className="font-mono">{`Loading: ${loading}`}</p> */}
-        </div>
-        <div className="p-2 pt-0 text-primary-300 text-bold hidden">
-          {/* {displayMessage} */}
-        </div>
       </div>
     </Fragment>
   );
